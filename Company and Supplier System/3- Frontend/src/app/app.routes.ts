977bd325ce320ba0e36dd9397b1ef42d72365d3b
Igor Palhares <igor.1palhares@hotmail.com>
2025-09-23 23:29:53 -0300

    Fix: Rename paths and config library

--- Company and Supplier System/3- Frontend/src/app/app.routes.ts
+++ Company and Supplier System/3- Frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CompanySupplier } from './Pages/company-supplier/company-supplier';
+import { Company } from './Pages/company/company';
+import { Supplier } from './Pages/supplier/supplier';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', component: CompanySupplier },
+    { path: 'company', component: Company },
+    { path: 'supplier', component: Supplier },
+];
